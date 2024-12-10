a8a9244ddcd954a0a9bbcac9eacc61d173827ceb
KAMAbee <kamil.akbarov.94@gmail.com>
2024-12-10 19:21:42 +0500

    1 assingment

---
+++ main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"encoding/json"
+    "fmt"
+    "net/http"
+)
+
+type Request struct{
+	Message string `json:"message"`
+}
+
+type Response struct{
+	Status string `json:"status"`
+	Message string `json:"message"`
+}
+
+
+func handleRequest(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	if r.Method != http.MethodPost && r.Method != http.MethodGet {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+
+		json.NewEncoder(w).Encode(Response{
+			Status:  "fail",
+			Message: "Method not allowed. Only GET and POST are supported.",
+		})		
+		return
+	}
+
+	var requestData Request
+	json.NewDecoder(r.Body).Decode(&requestData)
+	
+	if requestData.Message == "" {
+		w.WriteHeader(http.StatusBadRequest)
+		json.NewEncoder(w).Encode(Response{
+			Status:  "fail",
+			Message: "Invalid JSON message",
+		})
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(Response{
+		Status:  "success",
+		Message: "Data successfully received",
+	})
+
+}
+
+func main() {
+    http.HandleFunc("/", handleRequest)
+    fmt.Println("Server is running")
+    http.ListenAndServe("localhost:8080", nil)
+}
